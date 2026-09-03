f4171577fdb4d87be7c956130aabd4b408f80a9c
Springfall89 <150023618+Springfall89@users.noreply.github.com>
2026-09-03 15:31:29 +0700

    Declare getComputerChoice func with funcs inside

--- js/global.js
+++ js/global.js
@@ -1,3 +1,16 @@
 const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
+
+// declare getComputerChoice func
+// declare getRandomInt func
+// return math floor of math random method with int 3
+
+function getComputerChoice()
+{
+    function getRandomInt(max)
+    {
+        return Math.floor(Math.random() * max);
+    }
+    return getRandomInt(3);
+}
