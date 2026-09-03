159677db87c94216eda48ee1f4086a001053aa9b
Springfall89 <150023618+Springfall89@users.noreply.github.com>
2026-09-03 15:40:50 +0700

    Add switches for random int cases

--- js/global.js
+++ js/global.js
@@ -2,15 +2,21 @@ const ROCK = "rock";
 const PAPER = "paper";
 const SCISSORS = "scissors";
 
-// declare getComputerChoice func
-// declare getRandomInt func
-// return math floor of math random method with int 3
-
 function getComputerChoice()
 {
     function getRandomInt(max)
     {
         return Math.floor(Math.random() * max);
     }
-    return getRandomInt(3);
+
+switch (getRandomInt(3))
+{
+    case 0: {
+        return ROCK;
+    } case 1: {
+        return PAPER;
+    } case 2: {
+        return SCISSORS;
+    }
+}
 }
