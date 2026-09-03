d2dc2803532958d7e2ff83ae56ae496e05629820
Springfall89 <150023618+Springfall89@users.noreply.github.com>
2026-09-03 16:01:48 +0700

    Add getHumanChoice func with switches and prompt

--- js/global.js
+++ js/global.js
@@ -20,3 +20,22 @@ switch (getRandomInt(3))
     }
 }
 }
+
+function getHumanChoice()
+{
+    let choice = prompt("Enter your move:");
+    switch (choice.toLowerCase())
+    {
+        case "rock": {
+            return ROCK;
+        } case "paper": {
+            return PAPER;
+        } case "scissors": {
+            return SCISSORS;
+        } default: {
+            alert("Try Again!");
+            getHumanChoice();
+            break;
+        }
+    }
+}
