a2c914712b87318798aea5910d3efcbd93df9a00
Springfall89 <150023618+Springfall89@users.noreply.github.com>
2026-09-04 13:50:57 +0700

    Add safeguards for valid values for playGame

--- js/global.js
+++ js/global.js
@@ -125,35 +125,62 @@ function showScore()
 function playGame(scoreMax)
 {   
     let roundNum = 1;
-    alert(`Game Start!\nYou vs. Computer!\nBest of ${scoreMax}!`)
-    while ((computerScore || humanScore) <= scoreMax)
+    
+    if (scoreMax >= 1)
     {
-        alert(`Round ${roundNum}!`);
-        let roundResult = playRound();
-            if (roundResult === null)
-            {
-                alert(`Game Over!\nYou cancelled the game.\nScores reset to zero.`);
-                computerScore = 0;
-                humanScore = 0;
-                break;
-            }
-        alert(showScore());
-        ++roundNum;
-        if ((computerScore || humanScore) === scoreMax)
+        alert(`Game Start!\nYou vs. Computer!\nBest of ${scoreMax}!`)
+        while ((computerScore || humanScore) <= scoreMax)
         {
-            if (computerScore > humanScore)
+            alert(`Round ${roundNum}!`);
+            let roundResult = playRound();
+                if (roundResult === null)
+                {
+                    alert(`Game Over!\nYou cancelled the game.\nScores reset to zero.`);
+                    computerScore = 0;
+                    humanScore = 0;
+                    break;
+                }
+            alert(showScore());
+            ++roundNum;
+            if ((computerScore || humanScore) === scoreMax)
             {
-                alert(`Game Over!\n\n${showScore()}\n\nThe Computer wins!`);
-                console.log(`Game Over!\n\n${showScore()}\n\nThe Computer wins!`);
-                break;
-            }
-            else if (computerScore < humanScore)
-            {
-                alert(`Game Over!\n\n${showScore()}\n\nThe Computer wins!`);
-                console.log(`Game Over!\n\n${showScore()}\n\nYou win!`);
-                break;
+                if (computerScore > humanScore)
+                {
+                    alert(`Game Over!\n\n${showScore()}\n\nThe Computer wins!`);
+                    console.log(`Game Over!\n\n${showScore()}\n\nThe Computer wins!`);
+                    break;
+                }
+                else if (computerScore < humanScore)
+                {
+                    alert(`Game Over!\n\n${showScore()}\n\nThe Computer wins!`);
+                    console.log(`Game Over!\n\n${showScore()}\n\nYou win!`);
+                    break;
+                }
             }
         }
+    }
+    else if (scoreMax === 0)
+    {
+        alert(`Game Start!\nYou vs. Computer!\nInfinite Rounds!\n\nCancel to end the game.`);
+        while ((computerScore || humanScore) <= scoreMax)
+        {
+            alert(`Round ${roundNum}!`);
+            let roundResult = playRound();
+                if (roundResult === null)
+                {
+                    alert(`Game Over!\nYou cancelled the game.\nScores reset to zero.`);
+                    computerScore = 0;
+                    humanScore = 0;
+                    break;
+                }
+            alert(showScore());
+            ++roundNum;
+        }
+    }
+    else
+    {
+        alert('Invalid Argument!\n\nOnly one argument is accepted.\nAcceptable values include:\n\n- Natural numbers for finite rounds.\n- Zero or "Infinity" for infinite rounds.');
+        console.error('Invalid Argument!\n\nOnly one argument is accepted.\nAcceptable values include:\n\n- Natural numbers for finite rounds.\n- Zero or "Infinity" for infinite rounds.');
     }
 }
 
